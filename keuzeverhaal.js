a84a894290aefd5efea31f8600aa7f5a5f1fc2f8
Alex <144677432+notsosaltyasiankid@users.noreply.github.com>
2023-10-17 10:50:49 +0200

    Add files via upload

---
+++ keuzeverhaal.js
@@ -0,0 +1,17 @@
+const readlineSync = require('readline-sync'); {
+const player = {
+  naam: ' ',
+}
+
+var naam = readlineSync.question ("Wat is je naam?")
+console.log (`Hallo, ${naam}! welkom bij mijn keuzeverhaal`)
+console.log("")
+console.log(`Dit verhaal gaat over ${naam} die probeert te overleven tegen een slasher.`)
+console.log("")
+console.log("Je bent op vakantie met een paar vrienden in zwitserland diep in het bos. Jij en je vrienden hebben een vakantie huis gehuurt voor een paar dagen. Het is nu 22:00 en al je vrienden zijn al aan het slapen want ze willen morgen vroeg weggaan. Jij bent nog de laatste die gaat slapen dus je doet alles maar dicht")
+console.log("")
+
+
+    
+
+}
